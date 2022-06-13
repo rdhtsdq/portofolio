82230663a2cf30648d8ed1d1fb5419441e65c555
= <=>
2022-06-13 15:49:27 +0700

    new update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
+document.body.addEventListener("mousemove",e => {
+  const X = e.clientX;
+  const Y = e.clientY;
+
+  gsap.to('.cursor',{
+    x:X,
+    y:Y,
+    stagger:-0.1
+  })
+})
+
+
 gsap.registerPlugin(ScrollTrigger)
+
+const tl = gsap.timeline()
+
+tl.to(".panel",{x:-window.innerWidth()})
+
 
 gsap.from(".navbar",{duration:1,y:-100,opacity:0})
 gsap.from(".profile",{duration:1,y:-50,opacity:0,delay:0.5})
@@ -10,10 +27,12 @@ gsap.from(".about-text",{
   x:-100,
   opacity:0,
   scrollTrigger:{
+    scrub:true,
     start:"top 80%",
     end:"top 40%",
     trigger:".about-text",
-  }
+    toggleActions:"play",
+  },
 })
 
 gsap.from(".about-profile",{
@@ -24,11 +43,12 @@ gsap.from(".about-profile",{
   scrollTrigger:{
     start:"top 80%",
     end:"top center",
-    trigger:".about-text"
+    trigger:".about-text",
+    scrub:true
   }
 })
 
-gsap.from("about-desc",{
+gsap.from(".about-desc",{
   duration:1,
   x:-100,
   opacity:0,
@@ -36,7 +56,25 @@ gsap.from("about-desc",{
   scrollTrigger:{
     start:"top 80%",
     end:"top 60%",
-    trigger:".about-profile"
+    trigger:".about-desc",
+    scrub:true
+
   }
 })
+
+
+// gsap.utils.toArray(".panel").forEach((panel, i) => {
+//   ScrollTrigger.create({
+//     trigger: panel,
+//     start: "top top", 
+//     pin: true, 
+//     pinSpacing: false 
+//   });
+// });
+
+
+// ScrollTrigger.create({
+//   snap: 1 / 4 // snap whole page to the closest section!
+// });
+
 
