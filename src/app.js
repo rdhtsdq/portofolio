4cbda91eecf176e22a4b28f9e9e0583620ed2a57
= <=>
2022-06-14 11:06:23 +0700

    Add scroll effect

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 gsap.registerPlugin(ScrollTrigger)
 
-gsap.from(".navbar",{duration:1,y:-100,opacity:0})
+// hero section
+
+// gsap.from(".navbar",{duration:1,y:-100,opacity:0})
 gsap.from(".profile",{duration:1,y:-50,opacity:0,delay:0.5})
 gsap.from(".text-hero",{duration:1,y:-50,opacity:0,delay:0.8})
 gsap.from(".icon-hero",{duration:1,y:-30,opacity:0,delay:1})
+
+// about section
 
 gsap.from(".about-text",{
   duration:1,
   x:-100,
   opacity:0,
   scrollTrigger:{
-    scrub:true,
+    // scrub:true,
     start:"top 80%",
     end:"top 40%",
     trigger:".about-text",
@@ -27,7 +31,7 @@ gsap.from(".about-profile",{
     start:"top 80%",
     end:"top center",
     trigger:".about-text",
-    scrub:true
+    // scrub:true
   }
 })
 
@@ -40,10 +44,47 @@ gsap.from(".about-desc",{
     start:"top 80%",
     end:"top 60%",
     trigger:".about-desc",
-    scrub:true
+    // scrub:true
 
   }
 })
+
+// skil section
+// gsap.from(".skil-panel",{
+//   duration:1,
+//   opacity:0,
+//   y:100,
+//   scrollTrigger:{
+//     start:"top 70%",
+//     end:"top 40%",
+//     trigger:".skil-panel",
+//   }
+// })
+
+gsap.from(".skil-text",{
+  duration:1,
+  y:50,
+  opacity:0,
+  ease:"back",
+  scrollTrigger:{
+    trigger:".skil-text",
+    start:"top 80%",
+  }
+})
+
+gsap.from(".skil-card",{
+  duration:1,
+  delay:0.75,
+  opacity:0,
+  y:60,
+  ease:"back",
+  scrollTrigger:{
+    trigger:".skil-text"
+  }
+})
+
+
+// cursor
 
 document.body.addEventListener("mousemove",e => {
   const X = e.clientX;
@@ -56,19 +97,19 @@ document.body.addEventListener("mousemove",e => {
   })
 })
 
+
+gsap.utils.toArray(".panel").forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "top top", 
+    pin: true, 
+    pinSpacing: false 
+  });
+});
+
 const tl = gsap.timeline()
 tl.to(".panel",{x:-window.innerWidth()})
 
-// gsap.utils.toArray(".panel").forEach((panel, i) => {
-//   ScrollTrigger.create({
-//     trigger: panel,
-//     start: "top top", 
-//     pin: true, 
-//     pinSpacing: false 
-//   });
-// });
-
-
-// ScrollTrigger.create({
-//   snap: 1 / 4 // snap whole page to the closest section!
-// });
+ScrollTrigger.create({
+  snap: 1 / 8 // snap whole page to the closest section!
+});
