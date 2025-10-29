46303093f1c574c5ecff2878ef3ca2c1e9680988
= <=>
2025-10-29 17:16:42 +0700

    update

---
+++ _next/static/-_5b9I_Kfu6UpO50jDYMe/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/240dbb3cc00969ee.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
