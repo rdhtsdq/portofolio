6b3e685ebd4945743a31b72e330240d2e204b758
= <=>
2025-10-29 17:33:56 +0700

    updates

--- _next/static/chunks/240dbb3cc00969ee.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/e376664127dd86b0.js",
-  "static/chunks/b710d25d5eec0632.js",
-  "static/chunks/turbopack-910a36f4be18b5f8.js"
-])
--- _next/static/chunks/31eab55e2f2d2df4.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/7b926a71f02c2414.js",
-  "static/chunks/b710d25d5eec0632.js",
-  "static/chunks/turbopack-86ea4b2706832ecb.js"
-])
